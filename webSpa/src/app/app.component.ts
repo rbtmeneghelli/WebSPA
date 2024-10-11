2aedc43c2c3d0081721be5e35c7176e90ad418be
Roberto Meneghelli <roberto.mng.89@gmail.com>
2024-10-11 17:28:04 -0300

    Versão 1.1 - Iniciado dev da tela de consulta
    Commit (Versão 1.1 - Iniciado dev da tela de consulta)

--- webSpa/src/app/app.component.ts
+++ webSpa/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from '../modules/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'web-spa';
+  title = 'webSpa';
 }
