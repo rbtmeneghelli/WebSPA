9d67b5af35f69c28ab3c4d85e4981a8a7f5e66d5
Roberto Meneghelli <roberto.mng.89@gmail.com>
2024-10-16 00:18:14 -0300

    Versão 3.1 - Add config de scroll
    Commit(Versão 3.1 - Add config de scroll)

--- webSpa/src/app/app.config.ts
+++ webSpa/src/app/app.config.ts
@@ -1,15 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { InMemoryScrollingFeature, InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { LoaderInterceptor } from '../core/interceptor/loader.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(APP_ROUTES), 
+    provideRouter(APP_ROUTES, inMemoryScrollingFeature), 
     provideAnimationsAsync(),
     { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
     { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
