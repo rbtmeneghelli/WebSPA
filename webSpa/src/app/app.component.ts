0be4a16b8e117e9e0ab0b203f341098bb5ab0081
Roberto Meneghelli <roberto.mng.89@gmail.com>
2024-11-11 23:27:27 -0300

    Versão 3.9 - Implementado rxjs takeUnilDestroyed
    Commit (Versão 3.9 - Implementado rxjs takeUnilDestroyed)

--- webSpa/src/app/app.component.ts
+++ webSpa/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingComponent } from '../shared/components/loading/loading.component';
-import { fromEvent, map, merge, Observable, Observer } from 'rxjs';
+import { fromEvent, map, merge, Observable, Observer, takeUntil } from 'rxjs';
 import { SnackBarService } from '../core/services/snackBar.service';
 import { EnumActionMessage, EnumTypeMessage } from '../core/enums/enums';
 import { LayoutComponent } from '../modules/layout/layout.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -18,8 +19,11 @@ export class AppComponent implements OnInit {
   public title = 'webSpa';
   private snackBarService: SnackBarService = inject(SnackBarService);
 
+  //take >> Obtem uma unica resposta do observable, fazendo o observable ser encerrado após resposta.
+  //takeUntil >> Destroi o observable completamente sem necessidade de aguardar a resposta, assim que o componente for destruido. (Funcionalidade a partir do angular 15 ou inferior)
+  //takeUntilDestroyed >> Destroi o observable completamente sem necessidade de aguardar a resposta, assim que o componente for destruido. (Funcionalidade a partir do angular 16 ou superior)
   ngOnInit(): void {
-    this.createOnline().subscribe((isOnline: any) => {
+    this.createOnline().pipe(takeUntilDestroyed()).subscribe((isOnline: any) => {
       if (isOnline === false) {
         this.snackBarService.sendSnackBarNotification("Não foi possível prosseguir, por favor, verifique sua conexão à internet.", EnumTypeMessage.Personalized, EnumActionMessage.Info, true);
       }
